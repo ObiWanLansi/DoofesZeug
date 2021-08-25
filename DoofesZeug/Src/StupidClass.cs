d5cb13895db13e940b9a37737d066ae365645e27
ObiWanLansi <joerg.lanser@gmail.com>
2021-08-25 20:33:35 +0200

    StupidClass

---
+++ DoofesZeug/Src/StupidClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+
+namespace DoofesZeug
+{
+    public static class StupidClass
+    {
+        public static void DoStupidStuff()
+        {
+            Console.Out.WriteLine("Oops I Did It Again!");
+        }
+    }
+}
