7874fca2eb7e161261493871c7344453df62cdb9
ObiWanLansi <joerg.lanser@gmail.com>
2021-09-02 10:53:38 +0200

    Confifure NUnitTest FrameWork

---
+++ DoofesZeug.UnitTests/Src/Formalities/TestNamespaces.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+
+
+namespace DoofesZeug.Formalities
+{
+    public sealed class TestNamespaces
+    {
+        [Test]
+        public void Test_Namespaces()
+        {
+
+        }
+    }
+}
